3023aab5402df1b4763c9aa7daae5df74a60e9c1
Muhammadjon <tokhirov.mukhammadjon@gmail.com>
2022-05-11 20:47:09 +0500

    task 1 remastered

--- task1/Task 1.playground/Pages/Task 1.xcplaygroundpage/Contents.swift
+++ task1/Task 1.playground/Pages/Task 1.xcplaygroundpage/Contents.swift
@@ -2,173 +2,82 @@
 import UIKit
 import Foundation
 
-// 1
-
-//func secondsIn(year: Int) -> Int {
-//    let minSecond = 60
-//    let hourSecond = minSecond * 60
-//    let daySecond = hourSecond * 24
-//    return daySecond * 365 * year
-//}
-//
-//print(secondsIn(year: 1))
-
-
-// 2
-
-
-//func getCustom(widh: Int, height: Int) -> Int {
-//var numberOfPixel = widh + height * 2
-//
-//    return numberOfPixel
-//}
-//
-//print(getCustom(widh: 1080, height: 720))
-
-
-// 3
-
-//func custom(total: Int, difference: Int) -> Int {
-//    let a = (total + difference) / 2
-//    let b = total - a
-//    print("A = \(a) B = \(b) Total = \(a + b)")
-//    return  a + b
-//}
-//print( custom(total: 25, difference: 5))
-
-// 4
-
- 
-//var a = 2
-//var b = 3
-//// custom(a, b)
-//// pass by value                         manzili o'zgarmedi
-//// pass by reference                     manzili o'zgaradi
-//print(a, b)
-//
-//func swap(a: inout Int, b: inout Int) {
-//    let t = a
-//    a = b
-//    b = t
-//}
-//
-//swap(a: &a, b: &b)
-//
-//print(a, b)
-
-// 5
-
-//var salom = "Hi"
-//var janob = "Mr"
-//
-//func swap<T>(_ a: inout T, _ b: inout T) {
-//     (a, b) = (b, a)
-//}
-//
-//
-//
-//swap(&salom, &janob)
-//print(salom, janob)
- 
- 
-
-// 6
-
-
-//func digit(a: Int) -> Int {
-//
-//    return a % 10
-//}
-//
-//print(digit(a: 2004))
-
-// 7
-
-//func age(rockyAge: Double ) -> Double {
-//
-//    var humanAge = Double()
-//    humanAge = rockyAge/7
-//    humanAge *= 12
-//    if humanAge > 12 {
-//        humanAge /= 12
-//        print( Int(humanAge), "Year HumanAge" )
-//    } else {
-//        print( Int(humanAge), "Month HumanAge" )
-//    }
-//
-//    return humanAge
-//}
-//  print(age(rockyAge: 12))
-
-
-// 8
-
-//func age(x: Int, y: Int) -> Int {
-//  var bob = 12
-//  let alice = (bob + x) * y - x
-//
-//    return alice
-//}
-//
-//print(age(x: 3, y: 2))
-
-// 9
-
-
-//func fruit(apples: Double) -> Int {
-//    var orange = Int()
-//    var orangeMock = Double()
-//
-//    orangeMock = apples * 0.6
-//    orange = Int(orangeMock)
-//
-//    return orange
-//}
-//
-//    print(fruit(apples: 23))
-
-
-// 10
-
-
-//var numberOfBoys  = Double(5)
-//var numberOfGirls = Double(5)
-//
-//func clas(numberOfBoys: inout Double, numberOfGirls: inout Double){
-//
-//    var fullclass = numberOfBoys + numberOfGirls
-//        numberOfBoys = numberOfBoys / fullclass * 100
-//        numberOfGirls = numberOfGirls / fullclass * 100
-//}
-// clas(numberOfBoys: &numberOfBoys, numberOfGirls: &numberOfGirls)
-// print(Int(numberOfBoys), Int(numberOfGirls))
-
-     
-// 11
-
-//var sounds = String()
-//var number = Int(15)
-
-//func pling(sounds: inout String, number: inout Int) {
-//
-//    if number % 3 == 0 {
-//        sounds += "Pling"
-//    }
-//
-//    if number % 5 == 0 {
-//    sounds += "Plang"
-//
-//    }
-//
-//    if number % 7 == 0 {
-//    sounds += "Plong"
-//    }
-//
-//    if sounds.isEmpty {
-//        sounds = String(number)
-//    }
-//}
-//
-//pling(sounds: &sounds, number: &number)
-//print(sounds)
+class Application {
+    func problem1(year: Int) -> Int {
+        let minSecond = 60
+        let hourSecond = minSecond * 60
+        let daySecond = hourSecond * 24
+        return daySecond * 365 * year
+    }
     
+    func problem2(width: Int, height: Int) -> Int {
+        let numberOfPixel = width * height
+        return numberOfPixel
+    }
+    
+    func problem3(sum: Int, difference: Int) -> (a: Int, b: Int) {
+        let a = (sum + difference) / 2
+        let b = (sum - difference) / 2
+        
+        return  (a, b)
+    }
+    
+    func swap<T>(_ a: inout T, _ b: inout T) {
+        (a, b) = (b, a)
+    }
+    
+    func lastDigit(a: Int) -> Int {
+        a % 10
+    }
+    
+    func humanAge(dogAge: Int) -> Double {
+        Double(dogAge) / 7
+    }
+    
+    func age(x: Int, y: Int) -> Int {
+        let bob = 12
+        
+        let alice = (bob + x) * y - x
+        
+        return alice
+    }
+    
+    func problem8(_ x: Int) {
+        let maxExchangableApples = x / 5 * 5
+        let oranges = maxExchangableApples / 5 * 3
+        let apples = x - maxExchangableApples
+        
+        print("Get \(oranges) oranges form Bob")
+        print("Left \(apples) apples on basket")
+    }
+    
+    func clas(numberOfBoys: Double, numberOfGirls: Double) -> (boys: Int, girls: Int) {
+        let fullclass = numberOfBoys + numberOfGirls
+        let percentageOfBoys = numberOfBoys / fullclass * 100
+        let percentageOfGirls = numberOfGirls / fullclass * 100
+        
+        return (Int(percentageOfBoys), Int(percentageOfGirls))
+    }
+    
+    func pling(sounds: inout String, number: inout Int) {
+        
+        if number % 3 == 0 {
+            sounds += "Pling"
+        }
+        
+        if number % 5 == 0 {
+            sounds += "Plang"
+        }
+        
+        if number % 7 == 0 {
+            sounds += "Plong"
+        }
+        
+        if sounds.isEmpty {
+            sounds = String(number)
+        }
+    }
+}
+
+
+let app = Application()
--- task1/task1.md
+++ task1/task1.md
@@ -1,31 +1,44 @@
 # TASK1
-1.  Determine the number of seconds in a year and store the number in a variable named __secondInAYear__.
+
+1. Determine the number of seconds in a year and store the number in a variable named __secondInAYear__.
+
 2. Your are given the width and height of a screen in pixels. Calculate the total number of pixels on the screen and store the result in a variable named __numberOfPixels__.
+
 3. You are given the sum and the difference of two numbers. Find out the values of the original numbers and store them in variables __a__ and __b__.
+
 4. Given two variable __a__ and __b__, swap their values. That is the new value of __a __will become the old value of __b__ and vice versa.
+   
    > Solve with two way.
+
 5. You are given a number __a__. Print the last digit of __a__.
+
 6. You are given Rocky’s age in dog years. Print Rocky’s age in human years. You know that 1 human year is __7__ dog years.
+
 7. Everyone hates solving word problems by hand so let’s make a program to solve them for us.
    __x__ years from now Alice will be __y__ times older than her brother Bob. Bob is __12__ years old. How many years does Alice have?
+
 8. You have __x__ apples. Bob trades __3__ oranges for __5__ apples. He does not accept trades with cut fruit.
+   
    * How many oranges can you get from Bob and how many apples will you have left?
    * The number of apples you will have left should be stored in a variable named apples.
    * The number of oranges you will have after the trade should be stored in a variable named oranges.
-   
+     
+
 9. A class consists of __numberOfBoys__ boys and __numberOfGirls__ girls.
-   Print the percentage of boys in the class followed by the percentage of girls in the class.
-   The percentage should be printed rounded down to the nearest integer. For example **33.333333333333** will be printed as **33**.
-10. __Raindrops__
-	Convert a number to a string, the contents of which depend on the number's factors.
-	* If the number has 3 as a factor, output 'Pling'.
-	* If the number has 5 as a factor, output 'Plang'.
-	* If the number has 7 as a factor, output 'Plong'.
-	* If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
-	__*Examples*__
-	* 28's factors are 1, 2, 4, 7, 14, 28.
-	* In raindrop-speak, this would be a simple "Plong".
-	* 30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
-	* In raindrop-speak, this would be a "PlingPlang".
-	* 34 has four factors: 1, 2, 17, and 34.
-	* In raindrop-speak, this would be "34".
+      Print the percentage of boys in the class followed by the percentage of girls in the class.
+      The percentage should be printed rounded down to the nearest integer. For example **33.333333333333** will be printed as **33**.
+   
+   10. __Raindrops__
+           Convert a number to a string, the contents of which depend on the number's factors.
+           * If the number has 3 as a factor, output 'Pling'.
+           * If the number has 5 as a factor, output 'Plang'.
+           * If the number has 7 as a factor, output 'Plong'.
+           * If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
+           __*Examples*__
+           * 28's factors are 1, 2, 4, 7, 14, 28.
+           * In raindrop-speak, this would be a simple "Plong".
+           * 30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
+           * In raindrop-speak, this would be a "PlingPlang".
+           * 34 has four factors: 1, 2, 17, and 34.
+           * In raindrop-speak, this would be "34".
+       
